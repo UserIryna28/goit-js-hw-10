bb68e120600aa6dce202076ae6bd0a47443dde70
UserIryna28 <Ltdcom@ukr.net>
2023-05-16 10:14:58 +0300

    Add changes to files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
+import Notiflix from 'notiflix';
+
+const input = document.querySelector("#search-box")
+const countryList = document.querySelector(".country-list")
+const countryInfo = document.querySelector(".country-info")
+
 const DEBOUNCE_DELAY = 300;
